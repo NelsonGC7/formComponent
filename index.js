55407c77f7936b821827e5e0f8322c63b59a68e0
PittnoiR <omargarza394@gmail.com>
2024-01-10 22:47:04 -0600

    ya solo falta la validacion real

--- index.js
+++ index.js
@@ -1,70 +1,79 @@
 const formulari =  document.forms[0];
 const [Iname,Ilasn,Iemail,Ipass,Posbtn] = formulari;
-console
+
+
+const validacion = ()=>{
+  let name,lasn,email,pass;
+  if(Iname.value.length>=4){
+    name = Iname.value;
+    Iname.classList.remove('err');
+    document.getElementById('name').classList.remove('errorA');  
+  }else{
+     Iname.classList.add('err')
+     document.getElementById('name').classList.add('errorA')
+     return false
+
+  };
+  
+  if(Ilasn.value.length>4){
+     lasn = Ilasn.value;
+     Ilasn.classList.remove('err');
+     document.getElementById('last').classList.remove('errorA')
+     
+   }else{
+     Ilasn.classList.add('err');
+     document.getElementById('last').classList.add('errorA')
+     return false
+   }
+   
+   if(Iemail.value.length>4){
+     email = Iemail.value;
+     Iemail.classList.remove('err');
+     document.getElementById('emai').classList.remove('errorA')
+     
+     
+   }else{
+      Iemail.classList.add('err')
+      document.getElementById('emai').classList.add('errorA')
+      
+   }
+   
+   if(Ipass.value.length>=8){
+     pass = Ipass.value;
+     Ipass.classList.remove('err')
+     document.getElementById('pas').classList.remove('errorA')
+   }else{
+      Ipass.classList.add('err')
+      document.getElementById('pas').classList.add('errorA')
+      return false
+      
+   
+   };
+   
+   datos = {
+    name :name,
+    last: lasn,
+    email:email,
+    passwd:pass,
+  };
+  
+  
+
+}
+
+
 formulari.addEventListener('submit',(e)=>{
      e.preventDefault();
-     let name,lasn,email,pass;
-     if(Iname.value.length>=4){
-       name = Iname.value;
-       Iname.classList.remove('err');
-       document.getElementById('name').classList.remove('errorA');  
-     }else{
-        Iname.classList.add('err')
-        document.getElementById('name').classList.add('errorA')
-        return false
- 
-     };
      
-     if(Ilasn.value.length>4){
-        lasn = Ilasn.value;
-        Ilasn.classList.remove('err');
-        document.getElementById('last').classList.remove('errorA')
-        
-      }else{
-        Ilasn.classList.add('err');
-        document.getElementById('last').classList.add('errorA')
-        return false
-      }
-      
-      if(Iemail.value.length>4){
-        email = Iemail.value;
-        Iemail.classList.remove('err');
-        document.getElementById('emai').classList.remove('errorA')
-        
-        
-      }else{
-         Iemail.classList.add('err')
-         document.getElementById('emai').classList.add('errorA')
-         return false
-      }
-      
-      if(Ipass.value.length>=8){
-        pass = Ipass.value;
-        Ipass.classList.remove('err')
-        document.getElementById('pas').classList.remove('errorA')
-      }else{
-         Ipass.classList.add('err')
-         document.getElementById('pas').classList.add('errorA')
-         return false
-         
-      
-      }
-      
-      
-      datos = {
-        name :name,
-        last: lasn,
-        email:email,
-        passwd:pass  
-      }   
-      
+     validacion()
+     formulari.reset();
      
     
-    
-    
-    
-    
-
-
+   
+      
+      
+     
+      
+     
 })
 
